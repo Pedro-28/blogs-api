a3da89f67da214f75130e1239e653200fa129f23
Pedro-28 <pedrohdp2017@gmail.com>
2022-09-23 15:58:25 -0300

    Requisito 02 concluido

---
+++ src/models/User.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+  const userTable = sequelize.define('User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      displayName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'users',
+      underscored: true,
+      timestamps: false,
+    });
+  return userTable;
+};
