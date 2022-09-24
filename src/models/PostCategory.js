5a22a0eb5e1c81b5f6658b48cfdff7f2fa5f107d
Pedro-28 <pedrohdp2017@gmail.com>
2022-09-23 22:18:08 -0300

    Requisito 11 concluido

---
+++ src/models/PostCategory.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const blogPostTable = sequelize.define('PostCategory',
+    {
+      postId: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      categoryId: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'posts_categories',
+      underscored: true,
+      timestamps: false,
+    });
+
+  blogPostTable.associate = ({ Category, BlogPost }) => {
+    Category.belongsToMany(BlogPost, {
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      as: 'blogPosts',
+      through: blogPostTable,
+    });
+
+    BlogPost.belongsToMany(Category, {
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+      as: 'categories',
+      through: blogPostTable,
+    });
+  };
+
+  return blogPostTable;
+};
